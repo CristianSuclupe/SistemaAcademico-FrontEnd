5e5132ba52cf3f9a909eb5a7c8ad4a4452dd6e85
Cristian Omar Suclupe Llontop <N00148478@upn.pe>
2024-04-04 21:19:15 -0500

    prueba

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,20 +1,23 @@
-import { LoginData } from '../types/auth';
-import { ENV } from '../utils/constants';
+import { LoginData } from "../types/auth";
+import { ENV } from "../utils/constants";
 
 export class Auth {
   login = async (data: LoginData) => {
     try {
       const url = `${ENV.API_URL}/${ENV.ENDPOINTS.AUTH.LOGIN}`;
+      console.log(url);
       const params = {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       };
+      console.log(params);
       const response = await fetch(url, params);
       if (!response) return null;
       const result = await response.json();
+      console.log(result);
       return result;
     } catch (error) {
       throw new Error();
